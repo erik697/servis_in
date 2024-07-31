945f3e57c3725543440558f2202d6fc16b6d6d82
erik suardiana <eriksuardiana644@gmail.com>
2024-07-31 12:51:00 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faPhone, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 import Image from "next/image";
+
+let categories = [
+  {name : 'Electrician'},
+  {name : 'Builder'},
+  {name : 'AC'},
+  {name : 'Plumber'},
+  {name : 'Computer'},
+  {name : 'Car'},
+  {name : 'Phone'},
+]
+
+let products = [
+  {name : 'Budi Setiawan', category : 'AC' },
+  {name : 'Rudi Tabuti', category : 'AC'},
+  {name : 'Laksman', category : 'AC'},
+  {name : 'Rio Aditya', category : 'AC'},
+  {name : 'Rahmat', category : 'AC'},
+  {name : 'Ken', category : 'AC'},
+  {name : 'Kyo', category : 'AC'},
+]
+const Categories = ({name} : {name : string}) => {
+return (
+  <div className="px-3 justify-center">
+            <div className="w-16 h-16 rounded-full bg-slate-200 flex flex-wrap items-center justify-center">
+<FontAwesomeIcon width={25} icon={faPhone}/>
+            </div>
+            <p className="text-center text-sm">{name}</p>
+        </div>
+)
+}
+
+const Products = ({name, category} : {name : string, category:string}) =>{
+return (
+  <div className="shadow-lg bg-slate-50 rounded-md">
+  <Image
+  width={0}
+  height={0}
+  sizes="100vw"
+  className="w-full h-52 bg-gray-500 rounded-md"
+src="https://media.istockphoto.com/id/1347475562/id/foto/teknisi-ti-terfokus-pencahayaan-memeriksa-perangkat-keras-komputer.jpg?s=1024x1024&w=is&k=20&c=LATeffs_LGrD4pyP44c4TcM-QsMbeVX2A6F1mzA9Ir8="
+alt="Picture of the author"
+/>
+  
+  <div className="p-2">
+  <h5 className="font-bold">{category}</h5>
+  <h5 className="font-bold">{name}</h5>
+  <p className="text-sm">star 4.5 | 8 people</p>
+  </div>
+  </div>
+)
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+   <main className="w-full">
+    {/* Profile */}
+    <div className="flex justify-between p-4">
+      <div className="flex">
+        <div className="">
+        <Image
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-10 h-10 rounded-full bg-slate-500"
+      src="https://media.istockphoto.com/id/1476170969/id/foto/potret-pemuda-siap-kerja-konsep-bisnis.jpg?s=1024x1024&w=is&k=20&c=4COVCz0wDi5Jo2cxQGOaA_I-fm2cy1iy9OWwx56R1xs="
+      alt="Picture of the author"
+    />
+        </div>
+        <div className="ml-4">
+          <p className="text-sm">Welcome !</p>
+          <p className="text-black">Budi Santoso</p>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      
+      <div className="">
+      <FontAwesomeIcon width={30} icon={faHeart} />
+      </div>
+    </div>
+    {/* Searching */}
+      <div className="flex bg-slate-100 mx-4 my-2 p-4 rounded-md shadow-lg">
+          <div className="h-4 w-4 mr-3">
+          <FontAwesomeIcon width={20} icon={faSearch} />
+          </div>
+          <input type="text" className="bg-transparent focus:outline-none" placeholder="Search" />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* adsense */}
+      <div className="">
+      <Image
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[90%] h-[90px] bg-red-50 mx-auto mt-4"
+      src="https://media.istockphoto.com/id/1411398003/id/foto/botol-semprot-plastik-kosong-dan-kemasan-di-atas-meja-kayu-putih-untuk-desain-kemasan.jpg?s=1024x1024&w=is&k=20&c=BL_sMfNwQqrdiNUi9nLSVvv2nGCwR1xB9CZWlYxowck="
+      alt="Picture of the author"
+    />
+      
       </div>
-    </main>
+      {/* categori */}
+      <div className="overflow-x-scroll flex flex-row my-4 w-full">
+        {categories && categories.map((item, key)=>(
+          <Categories name={item.name} />
+        ))}
+        
+       
+      </div>
+
+      {/* Most Populer */}
+      <>
+      <div className="flex justify-between px-4">  
+        <h3 className="font-bold">Most Populer</h3>
+        <div className="">See All</div>
+      </div>
+      <div className="overflow-x-scroll flex flex-row my-4 w-full">
+        <div className="px-3 justify-center">
+            <div className="rounded-full py-1 px-2 border-2 border-black ">
+            <p>See All</p>
+            </div>
+        </div>
+       
+      </div>
+      </>
+
+      {/* product */}
+      <div className="grid grid-cols-2 gap-3 w-full p-4">
+      {products && products.map((item, key)=>(
+        <Products name={item.name} category={item.category} />
+      ))
+
+      } 
+      
+      </div>
+
+      <div className="sticky bottom-0 left-0 bg-slate-100 w-full grid grid-cols-4 gap-2 border-t">
+    <div className="my-3">
+      <div className="flex flex-wrap items-center justify-center">
+      <FontAwesomeIcon width={25} icon={faHome} />
+      </div>
+    <p className="text-center">Home</p>
+    </div>
+    <div className="my-3">
+      <div className="flex flex-wrap items-center justify-center">
+    <FontAwesomeIcon width={25} icon={faUser} />
+    </div>
+    <p className="text-center">Profile</p>
+    
+    </div>
+    <div className="my-3">
+      <div className="flex flex-wrap items-center justify-center">
+    <FontAwesomeIcon width={25} icon={faHeart} />
+    </div>
+    <p className="text-center">Favorit</p>
+    
+    </div>
+    <div className="my-3">
+      <div className="flex flex-wrap items-center justify-center">
+    <FontAwesomeIcon width={25} icon={faBook} />
+    </div>
+    <p className="text-center">History</p>
+    </div>
+    
+      </div>
+   </main>
+   </>
   );
 }
